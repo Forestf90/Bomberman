fd292509668210e7f63c1eee5d06c636ced0ee82
Forestf <michalsliwa96@wp.pl>
2019-06-27 15:55:12 +0200

    poor movment

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ RIGHT = 'right'
 WINDOWWIDTH = 600
 WINDOWHEIGHT = 600
 
+TILEWIDTH = 20
+TILEHEIGHT = 20
+
 BACKGROUND = (107, 142, 35)
 
 pygame.init()
@@ -23,27 +26,65 @@ player = Player()
 
 def draw():
     s.fill(BACKGROUND)
-    s.blit(player.frontAnimation[0], player.frontAnimation[0].get_rect())
+    s.blit(player.animation[player.direction][player.frame], (player.posX*TILEWIDTH, player.posY*TILEHEIGHT,
+                                                              TILEWIDTH, TILEHEIGHT))
 
     pygame.display.update()
 
 
 def main():
     while True:
-        clock.tick(20)
-        for e in pygame.event.get():
-            if e.type == pygame.QUIT:
-                sys.exit(0)
-           # if e.type == pygame.KEYDOWN:
-                # if e.key == pygame.K_UP and pyton.kierunek != 1:
-                #     pyton.kierunek = 3
-                # elif e.key == pygame.K_DOWN and pyton.kierunek != 3:
-                #     pyton.kierunek = 1
-                # elif e.key == pygame.K_LEFT and pyton.kierunek != 2:
-                #     pyton.kierunek = 0
-                # elif e.key == pygame.K_RIGHT and pyton.kierunek != 0:
-                #     pyton.kierunek = 2
+        clock.tick(5)
+        keys = pygame.key.get_pressed()
+
+        temp = player.direction
+        if keys[pygame.K_DOWN]:
+            temp = 0
+            player.posY += 1
+        elif keys[pygame.K_RIGHT]:
+            temp = 1
+            player.posX += 1
+        elif keys[pygame.K_UP]:
+            temp = 2
+            player.posY -= 1
+        elif keys[pygame.K_LEFT]:
+            temp = 3
+            player.posX -= 1
+        if temp != player.direction:
+            player.frame = 0
+            player.direction = temp
+        else:
+            if player.frame == 2:
+                player.frame = 0
+            else:
+                player.frame += 1
         draw()
+        for e in pygame.event.get():
+                if e.type == pygame.QUIT:
+                    sys.exit(0)
+                # if e.type == pygame.KEYDOWN:
+                #     temp = player.direction
+                #     if e.key == pygame.K_DOWN:
+                #         temp = 0
+                #         player.posY += 1
+                #     elif e.key == pygame.K_RIGHT:
+                #         temp = 1
+                #         player.posX += 1
+                #     elif e.key == pygame.K_UP:
+                #         temp = 2
+                #         player.posY -= 1
+                #     elif e.key == pygame.K_LEFT:
+                #         temp = 3
+                #         player.posX -= 1
+                #     if temp != player.direction:
+                #         player.frame = 0
+                #         player.direction = temp
+                #     else:
+                #         if player.frame == 2:
+                #             player.frame = 0
+                #         else:
+                #             player.frame += 1
+                draw()
 
 
 main()
--- player.py
+++ player.py
@@ -1,19 +1,44 @@
 import pygame
 
 class Player:
-    posX=0
-    posY=0
-    direction=0
-    frontAnimation =[]
-    backAnimation =[]
-    leftAnimation=[]
-    rightAnimation=[]
-    deathAnimation=[]
+    posX = 0
+    posY = 0
+    direction = 0
+    frame = 0
+    animation = []
+
+    # frontAnimation = []
+    # backAnimation = []
+    # leftAnimation = []
+    # rightAnimation = []
+    # deathAnimation = []
 
     def __init__(self):
         self.load_animations()
 
     def load_animations(self):
-        self.frontAnimation.append(pygame.image.load('images/hero/pf0.png'))
-        self.frontAnimation.append(pygame.image.load('images/hero/pf1.png'))
-        self.frontAnimation.append(pygame.image.load('images/hero/pf2.png'))
+        front = []
+        back = []
+        left = []
+        right = []
+
+        front.append(pygame.image.load('images/hero/pf0.png'))
+        front.append(pygame.image.load('images/hero/pf1.png'))
+        front.append(pygame.image.load('images/hero/pf2.png'))
+
+        right.append(pygame.image.load('images/hero/pr0.png'))
+        right.append(pygame.image.load('images/hero/pr1.png'))
+        right.append(pygame.image.load('images/hero/pr2.png'))
+
+        back.append(pygame.image.load('images/hero/pd0.png'))
+        back.append(pygame.image.load('images/hero/pd1.png'))
+        back.append(pygame.image.load('images/hero/pd2.png'))
+
+        left.append(pygame.image.load('images/hero/pl0.png'))
+        left.append(pygame.image.load('images/hero/pl1.png'))
+        left.append(pygame.image.load('images/hero/pl2.png'))
+
+        self.animation.append(front)
+        self.animation.append(right)
+        self.animation.append(back)
+        self.animation.append(left)
