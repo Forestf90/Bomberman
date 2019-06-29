6d06be2c211779a5de9c361521a6b9a1985d6418
Forestf <michalsliwa96@wp.pl>
2019-06-29 14:41:38 +0200

    Plant bomb

---
+++ bomb.py
@@ -0,0 +1,9 @@
+
+
+class Bomb:
+    frame = 0
+
+    def __init__(self, r, x, y):
+        self.range = r
+        self.posX = x
+        self.posY = y
--- main.py
+++ main.py
@@ -22,6 +22,7 @@ pygame.display.set_caption('Bomberman')
 clock = pygame.time.Clock()
 
 player = Player()
+bombs = []
 
 map = [[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
@@ -46,10 +47,17 @@ block_img = pygame.transform.scale(block_img, (TILEWIDTH, TILEHEIGHT))
 box_img = pygame.image.load('images/terrain/box.png')
 box_img = pygame.transform.scale(box_img, (TILEWIDTH, TILEHEIGHT))
 
+bomb1_img = pygame.image.load('images/bomb/1.png')
+bomb1_img = pygame.transform.scale(bomb1_img, (TILEWIDTH, TILEHEIGHT))
+
+bomb2_img = pygame.image.load('images/bomb/2.png')
+bomb2_img = pygame.transform.scale(bomb2_img, (TILEWIDTH, TILEHEIGHT))
+
+bomb3_img = pygame.image.load('images/bomb/2.png')
+bomb3_img = pygame.transform.scale(bomb3_img, (TILEWIDTH, TILEHEIGHT))
+
 terrain_images = [grass_img, block_img, box_img]
-# terrain_images.append(grass_img)
-# terrain_images.append(block_img)
-# terrain_images.append(box_img)
+bomb_images = [bomb1_img, bomb2_img, bomb3_img]
 
 
 def draw():
@@ -58,6 +66,9 @@ def draw():
         for j in range(len(map[i])):
             #if map[i][j] != 0:
             s.blit(terrain_images[map[i][j]], (i*TILEWIDTH, j*TILEHEIGHT, TILEHEIGHT, TILEWIDTH))
+
+    for x in bombs:
+        s.blit(bomb_images[x.frame], (x.posX*TILEWIDTH, x.posY*TILEHEIGHT, TILEHEIGHT, TILEWIDTH))
 
     s.blit(player.animation[player.direction][player.frame],
            (player.posX*(TILEWIDTH/4), player.posY*(TILEHEIGHT/4), TILEWIDTH, TILEHEIGHT))
@@ -115,6 +126,9 @@ def main():
                 player.frame = 0
             else:
                 player.frame += 1
+        if keys[pygame.K_SPACE]:
+            bombs.append(player.plant_bomb())
+
         draw()
         for e in pygame.event.get():
             if e.type == pygame.QUIT:
