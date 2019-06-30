e3d312cbdfb6921af3480f8641cf7a75ba5adf87
Forestf <michalsliwa96@wp.pl>
2019-06-30 12:52:02 +0200

    Explode

--- explosion.py
+++ explosion.py
@@ -9,6 +9,27 @@ class Explosion:
         self.time = 300
         self.frame = 0
 
-    def explode(self , map):
+    def explode(self, map):
 
         self.sectors.append([self.sourceX, self.sourceY])
+
+        for x in range(1, self.range):
+            if map[self.sourceX + x][self.sourceY] == 1:
+                break
+            elif map[self.sourceX+x][self.sourceY] == 0:
+                self.sectors.append([self.sourceX+x, self.sourceY])
+        for x in range(1, self.range):
+            if map[self.sourceX - x][self.sourceY] == 1:
+                break
+            elif map[self.sourceX-x][self.sourceY] == 0:
+                self.sectors.append([self.sourceX-x, self.sourceY])
+        for x in range(1, self.range):
+            if map[self.sourceX][self.sourceY + x] == 1:
+                break
+            elif map[self.sourceX][self.sourceY+x] == 0:
+                    self.sectors.append([self.sourceX, self.sourceY+x])
+        for x in range(1, self.range):
+            if map[self.sourceX][self.sourceY - x] == 1:
+                break
+            elif map[self.sourceX][self.sourceY-x] == 0:
+                self.sectors.append([self.sourceX, self.sourceY-x])
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame, sys, random
 from player import Player
+from explosion import Explosion
 
 UP = 'up'
 DOWN = 'down'
@@ -23,6 +24,7 @@ clock = pygame.time.Clock()
 
 player = Player()
 bombs = []
+explosions =[]
 
 map = [[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
@@ -56,8 +58,18 @@ bomb2_img = pygame.transform.scale(bomb2_img, (TILEWIDTH, TILEHEIGHT))
 bomb3_img = pygame.image.load('images/bomb/3.png')
 bomb3_img = pygame.transform.scale(bomb3_img, (TILEWIDTH, TILEHEIGHT))
 
+explosion1_img = pygame.image.load('images/explosion/1.png')
+explosion1_img = pygame.transform.scale(explosion1_img, (TILEWIDTH, TILEHEIGHT))
+
+explosion2_img = pygame.image.load('images/explosion/2.png')
+explosion2_img = pygame.transform.scale(explosion2_img, (TILEWIDTH, TILEHEIGHT))
+
+explosion3_img = pygame.image.load('images/explosion/3.png')
+explosion3_img = pygame.transform.scale(explosion3_img, (TILEWIDTH, TILEHEIGHT))
+
 terrain_images = [grass_img, block_img, box_img, grass_img]
 bomb_images = [bomb1_img, bomb2_img, bomb3_img]
+explosion_images = [explosion1_img, explosion2_img, explosion3_img]
 
 
 def draw():
@@ -69,6 +81,10 @@ def draw():
 
     for x in bombs:
         s.blit(bomb_images[x.frame], (x.posX*TILEWIDTH, x.posY*TILEHEIGHT, TILEHEIGHT, TILEWIDTH))
+
+    for y in explosions:
+        for x in y.sectors:
+            s.blit(explosion_images[y.frame], (x[0]*TILEWIDTH, x[1]*TILEHEIGHT, TILEHEIGHT, TILEWIDTH))
 
     s.blit(player.animation[player.direction][player.frame],
            (player.posX*(TILEWIDTH/4), player.posY*(TILEHEIGHT/4), TILEWIDTH, TILEHEIGHT))
@@ -139,6 +155,9 @@ def main():
             b.update(dt)
             if b.time < 1:
                 map[b.posX][b.posY] = 0
+                exp_temp = Explosion(b.posX, b.posY, b.range)
+                exp_temp.explode(map)
+                explosions.append(exp_temp)
                 bombs.remove(b)
 
 main()
