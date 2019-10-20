c73347530d6d05b18a83dcf697e14693fa5b8884
Forestf <michalsliwa96@wp.pl>
2019-10-20 13:19:11 +0200

    Add map generation test

--- tests/map_generation_test.py
+++ tests/map_generation_test.py
@@ -1,9 +1,27 @@
 import unittest
 import game
 
+
 class MyTestCase(unittest.TestCase):
-    def map_generation_number_of_blocks(self):
-        self.assertEqual(True, False)
+    def map_generation_corners(self):
+        game.generate_map()
+        self.assertEqual(game.map[1][1], 1)
+        self.assertEqual(game.map[1][2], 1)
+        self.assertEqual(game.map[2][1], 1)
+
+        l = len(map)
+
+        self.assertEqual(game.map[l-1][1], 1)
+        self.assertEqual(game.map[l-1][2], 1)
+        self.assertEqual(game.map[l-2][1], 1)
+
+        self.assertEqual(game.map[1][l-1], 1)
+        self.assertEqual(game.map[1][l-2], 1)
+        self.assertEqual(game.map[2][l-1], 1)
+
+        self.assertEqual(game.map[l - 1][l-1], 1)
+        self.assertEqual(game.map[l - 1][l-2], 1)
+        self.assertEqual(game.map[l - 2][l-1], 1)
 
 
 if __name__ == '__main__':
